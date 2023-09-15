69834d51c62fe895acdb7ee6f325117b9ed8c1f1
Saurahtayare <Saurbh@gmail.com>
2023-09-15 14:30:03 +0530

    modifled

--- Divid.java
+++ Divid.java
@@ -3,7 +3,7 @@ public class divid {
 	public static void main(String[] args) {
 		int a=10;
 		int b=20;
-		System.out.println("the divid is" +(a/b));
+		System.out.println("the final divid is" +(a/b));
 
 	}
 
